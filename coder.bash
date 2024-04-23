baab49a7ee00a6b16cf78034c7b37318a588ea68
pablolorenzos <79167240+pablolorenzos@users.noreply.github.com>
2024-04-23 16:54:13 +0200

    Create coder.bash

---
+++ coder.bash
@@ -0,0 +1,8 @@
+GNU nano 6.2                                    coder.bash                                             
+./x264 --output medium_config.264 --fps 24 --preset slow --bitrate 600 --vbv-maxrate 4800 --vbv-bufsize >
+
+
+./gpac_public/bin/gcc/MP4Box -add medium_config.264 -fps 24 Medium_config.mp4
+
+
+

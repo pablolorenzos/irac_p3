11546af4b3b9a296fe1f6c1761792d1176a8f2e8
pablolorenzos <79167240+pablolorenzos@users.noreply.github.com>
2024-04-26 17:44:18 +0200

    Update coder.bash

--- coder.bash
+++ coder.bash
@@ -2,7 +2,7 @@ GNU nano 6.2                                    coder.bash
 ./x264 --output low_config.264 --fps 24 --preset slow --bitrate 100 --vbv-maxrate 4800 --vbv-bufsize 9600 --min-keyint 48 --keyint 48 --scenecut 0 --no-scenecut --pass 1 --video-filter "resize:width=160,height=90" video_p3.mp4
 
 
-./gpac_public/bin/gcc/MP4Box -add medium_config.264 -fps 24 Medium_config.mp4
+./gpac_public/bin/gcc/MP4Box -add low_config.264 -fps 24 Low_config.mp4
 
 
 
